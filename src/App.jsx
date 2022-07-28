ca4804fe30031f11c8c6c05363b7e42509790a44
진평 김 <jinp12@playauto.co.kr>
2022-07-29 00:11:18 +0900

    commit: todo-delete and rewrite

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./App.css";
+import Template from "./todoComponentes/Template";
+import TodoList from "./todoComponentes/TodoList";
+import TodoInsert from "./todoComponentes/TodoInsert";
+import { MdAddCircle } from "react-icons/md";
+
+let nextId = 2;
+function App() {
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [insertToggle, setInsertToggle] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "할 일을 입력해주세요.",
+      checked: false,
+    },
+  ]);
+
+  const onCheckToggle = (id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  const onInsertToggle = () => {
+    if (selectedTodo) {
+      setSelectedTodo(null);
+    }
+    setInsertToggle((prev) => !prev);
+  };
+
+  const onInsertTodo = (text) => {
+    if (text === "") {
+      return alert("할 일을 입력해주세요.");
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false,
+      };
+      setTodos((todos) => todos.concat(todo));
+      nextId++;
+    }
+  };
+
+  const onChangeSelectedTodo = (todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const onRemove = (id) => {
+    onInsertToggle();
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
+  const onUpdate = (id, text) => {
+    onInsertToggle();
+    setTodos((todos) =>
+      todos.map((todo) => (todo.id === id ? { ...todo, text } : todo))
+    );
+  };
+
+  return (
+    <Template todoLength={todos.length}>
+      <TodoList
+        todos={todos}
+        onCheckToggle={onCheckToggle}
+        onInsertToggle={onInsertToggle}
+        setSelectedTodo={setSelectedTodo}
+        onChangeSelectedTodo={onChangeSelectedTodo}
+      />
+      <div className="add-todo-button" onClick={onInsertToggle}>
+        <MdAddCircle />
+      </div>
+      {insertToggle && (
+        <TodoInsert
+          selectedTodo={selectedTodo}
+          onInsertToggle={onInsertToggle}
+          onInsertTodo={onInsertTodo}
+          onRemove={onRemove}
+          onUpdate={onUpdate}
+        />
+      )}
+    </Template>
+  );
+}
+
+export default App;
