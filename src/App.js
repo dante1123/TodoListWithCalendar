362e0f7e7389ee8d06e4a59ac8a5fb23b49f9dc0
KimJinPyeong <jinp6327@hanmail.net>
2022-07-28 21:41:02 +0900

    commit-todoInsert

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import "./App.css";
 import Template from "./todoComponentes/Template";
 import TodoList from "./todoComponentes/TodoList";
+import TodoInsert from "./todoComponentes/TodoInsert";
 import { MdAddCircle } from "react-icons/md";
 
 function App() {
+  const [insertToggle, setInsertToggle] = useState(false);
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -22,12 +24,17 @@ function App() {
       checked: false,
     },
   ]);
+
+  const onInsertToggle = () => {
+    setInsertToggle((prev) => !prev);
+  };
   return (
     <Template todoLength={todos.length}>
       <TodoList todos={todos} />
-      <div className="add-todo-button">
+      <div className="add-todo-button" onClick={onInsertToggle}>
         <MdAddCircle />
       </div>
+      {insertToggle && <TodoInsert onInsertToggle={onInsertToggle} />}
     </Template>
   );
 }
--- src/todoComponentes/TodoInsert.js
+++ src/todoComponentes/TodoInsert.js
@@ -1,1 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdAddCircle } from "react-icons/md";
+import "./TodoInsert.css";
+
+const TodoInsert = ({ onInsertToggle }) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div>
+      <div className="background" onClick={onInsertToggle}></div>
+      <form>
+        <input
+          placeholder="please type"
+          value={value}
+          onChange={onChange}
+        ></input>
+        <button type="submit">
+          <MdAddCircle />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoInsert;
