206b1bedff0b18c1547fffadaf0206d712f6ca79
진평 김 <jinp12@playauto.co.kr>
2022-08-01 21:55:22 +0900

    commit -todoList_Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,18 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Template from "./todoComponentes/Template";
-import TodoList from "./todoComponentes/TodoList";
-import TodoInsert from "./todoComponentes/TodoInsert";
-import { MdAddCircle } from "react-icons/md";
+import TodoApp from "./todoComponentes/TodoApp";
+import TodoCalendar from "./todoComponentes/TodoCalendar";
 
-let nextId = 2;
 function App() {
-  const [selectedTodo, setSelectedTodo] = useState(null);
-  const [insertToggle, setInsertToggle] = useState(false);
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "할 일을 입력해주세요.",
-      checked: false,
-    },
-  ]);
-
-  const onCheckToggle = (id) => {
-    setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
-  };
-
-  const onInsertToggle = () => {
-    if (selectedTodo) {
-      setSelectedTodo(null);
-    }
-    setInsertToggle((prev) => !prev);
-  };
-
-  const onInsertTodo = (text) => {
-    if (text === "") {
-      return alert("할 일을 입력해주세요.");
-    } else {
-      const todo = {
-        id: nextId,
-        text,
-        checked: false,
-      };
-      setTodos((todos) => todos.concat(todo));
-      nextId++;
-    }
-  };
-
-  const onChangeSelectedTodo = (todo) => {
-    setSelectedTodo(todo);
-  };
-
-  const onRemove = (id) => {
-    onInsertToggle();
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
-  };
-  const onUpdate = (id, text) => {
-    onInsertToggle();
-    setTodos((todos) =>
-      todos.map((todo) => (todo.id === id ? { ...todo, text } : todo))
-    );
-  };
-
   return (
-    <Template todoLength={todos.length}>
-      <TodoList
-        todos={todos}
-        onCheckToggle={onCheckToggle}
-        onInsertToggle={onInsertToggle}
-        setSelectedTodo={setSelectedTodo}
-        onChangeSelectedTodo={onChangeSelectedTodo}
-      />
-      <div className="add-todo-button" onClick={onInsertToggle}>
-        <MdAddCircle />
-      </div>
-      {insertToggle && (
-        <TodoInsert
-          selectedTodo={selectedTodo}
-          onInsertToggle={onInsertToggle}
-          onInsertTodo={onInsertTodo}
-          onRemove={onRemove}
-          onUpdate={onUpdate}
-        />
-      )}
-    </Template>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/todoList" element={<TodoCalendar />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/todoComponentes/TodoApp.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./TodoApp.css";
+import Template from "./Template";
+import TodoList from "./TodoList";
+import TodoInsert from "./TodoInsert";
+import { MdAddCircle } from "react-icons/md";
+
+let nextId = 2;
+function TodoApp() {
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [insertToggle, setInsertToggle] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "할 일을 입력해주세요.",
+      checked: false,
+    },
+  ]);
+
+  const onCheckToggle = (id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  const onInsertToggle = () => {
+    if (selectedTodo) {
+      setSelectedTodo(null);
+    }
+    setInsertToggle((prev) => !prev);
+  };
+
+  const onInsertTodo = (text) => {
+    if (text === "") {
+      return alert("할 일을 입력해주세요.");
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false,
+      };
+      setTodos((todos) => todos.concat(todo));
+      nextId++;
+    }
+  };
+
+  const onChangeSelectedTodo = (todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const onRemove = (id) => {
+    onInsertToggle();
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
+  const onUpdate = (id, text) => {
+    onInsertToggle();
+    setTodos((todos) =>
+      todos.map((todo) => (todo.id === id ? { ...todo, text } : todo))
+    );
+  };
+
+  return (
+    <Template todoLength={todos.length}>
+      <TodoList
+        todos={todos}
+        onCheckToggle={onCheckToggle}
+        onInsertToggle={onInsertToggle}
+        setSelectedTodo={setSelectedTodo}
+        onChangeSelectedTodo={onChangeSelectedTodo}
+      />
+      <div className="add-todo-button" onClick={onInsertToggle}>
+        <MdAddCircle />
+      </div>
+      {insertToggle && (
+        <TodoInsert
+          selectedTodo={selectedTodo}
+          onInsertToggle={onInsertToggle}
+          onInsertTodo={onInsertTodo}
+          onRemove={onRemove}
+          onUpdate={onUpdate}
+        />
+      )}
+    </Template>
+  );
+}
+
+export default TodoApp;
